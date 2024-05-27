8809d1f38daaab2ca5954ce00714f6f2da9810a6
Dan Shevel <dan.shevel@trywonder.ai>
2024-05-27 15:48:48 +0300

    Created api manager

---
+++ API_Gateway/start_api.py
@@ -0,0 +1,26 @@
+import os
+
+import uvicorn
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from starlette.middleware.sessions import SessionMiddleware
+
+from Api_Manager import v1_router
+
+load_dotenv()
+app = FastAPI()
+app.add_middleware(
+    SessionMiddleware, secret_key=os.environ.get("APP_SECRET_KEY"), max_age=3600
+)
+
+app.include_router(v1_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8444,
+        ssl_keyfile="../key.pem",
+        ssl_certfile="../cert.pem",
+    )
