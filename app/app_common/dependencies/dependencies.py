1c0638637e7efda6eaf9e24c592bb3747de76b7b
Dan Shevel <dan.shevel@trywonder.ai>
2024-05-30 13:50:50 +0300

    Person Repository works with create_table, insert and exists (other methods were not tested)

--- app/app_common/dependencies/dependencies.py
+++ app/app_common/dependencies/dependencies.py
@@ -1,6 +1,14 @@
+from loguru import logger
+
 from ..repositories.persons_repository import PersonsRepository
-from ..postgres_connector import conn
+from ..postgres_connector import get_db_connection
 
 
 def persons_repository() -> PersonsRepository:
-    return PersonsRepository(conn=conn)
+    conn = get_db_connection()  # Establish the database connection
+    try:
+        with conn:
+            return PersonsRepository(conn=conn)
+    except Exception as e:
+        logger.error(f"Error establishing database connection: {e}")
+        return None
