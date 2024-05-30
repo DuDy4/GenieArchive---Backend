3aeab14732601d2df68f1a90a97776b0d4313ba8
Dan Shevel <dan.shevel@trywonder.ai>
2024-05-30 23:43:51 +0300

    added salesforce repository, and connected the auth and callback to save the data in the database. * exists() still not working well.

--- app/app_common/dependencies/dependencies.py
+++ app/app_common/dependencies/dependencies.py
@@ -2,6 +2,7 @@ from loguru import logger
 
 from ..repositories.persons_repository import PersonsRepository
 from ..repositories.interactions_repository import InteractionsRepository
+from ..repositories.salesforce_users_repository import SalesforceUsersRepository
 from ..postgres_connector import get_db_connection
 
 
@@ -20,6 +21,16 @@ def interactions_repository() -> InteractionsRepository:
     try:
         with conn:
             return InteractionsRepository(conn=conn)
+    except Exception as e:
+        logger.error(f"Error establishing database connection: {e}")
+        return None
+
+
+def salesforce_users_repository() -> SalesforceUsersRepository:
+    conn = get_db_connection()  # Establish the database connection
+    try:
+        with conn:
+            return SalesforceUsersRepository(conn=conn)
     except Exception as e:
         logger.error(f"Error establishing database connection: {e}")
         return None
--- app/services/salesforce.py
+++ app/services/salesforce.py
@@ -3,14 +3,22 @@ Module for interacting with the Salesforce API.
 """
 
 import os
+import uuid
+
 from dotenv import load_dotenv
 import requests
 from loguru import logger
 from requests_oauthlib import OAuth2Session
-from simple_salesforce import Salesforce
+from app_common.repositories.salesforce_users_repository import (
+    SalesforceUsersRepository,
+)
+from app_common.dependencies.dependencies import salesforce_users_repository
+
+from app_common.postgres_connector import get_db_connection
+
+from app_common.repositories.persons_repository import PersonsRepository
 
 load_dotenv()
-
 
 SELF_URL = os.environ.get("self_url", "https://localhost:8444")
 SALESFORCE_CLIENT_ID = os.environ.get("SALESFORCE_CLIENT_ID")
@@ -19,13 +27,20 @@ SALESFORCE_LOGIN_URL = os.environ.get("SALESFORCE_LOGIN_URL")
 SALESFORCE_REDIRECT_URI = SELF_URL + "/v1/salesforce/callback"
 SALESFORCE_TOKEN_URL = os.environ.get("SALESFORCE_TOKEN_URL")
 
+sf_users_repository = salesforce_users_repository()
+
 
 class SalesforceClient:
     """
     Class for interacting with the Salesforce API on behalf of a company.
     """
 
-    def __init__(self, access_token: str, instance_url: str):
+    def __init__(
+        self,
+        access_token: str,
+        instance_url: str,
+        sf_users_repository: SalesforceUsersRepository,
+    ):
         """
         Initializes the SalesforceClient with the given parameters.
 
@@ -35,6 +50,69 @@ class SalesforceClient:
         """
         self.access_token = access_token
         self.instance_url = instance_url
+
+
+class SalesforceAgent:
+    """
+    Class for communicating with salesforce and gather data to our database
+    """
+
+    def __init__(
+        self,
+        sf_client: SalesforceClient,
+        sf_users_repository: SalesforceUsersRepository,
+    ):
+        self.sf_client = sf_client
+        self.sf_users_repository = sf_users_repository
+
+    async def get_contacts(self):
+        """
+        Retrieve contacts from Salesforce.
+
+        Returns:
+        list: List of contact records.
+        """
+
+        url = f"{self.sf_client.instance_url}/services/data/v60.0/query/"
+        query = "SELECT Id, FirstName, LastName, Email FROM Contact LIMIT 100"
+        headers = {
+            "Authorization": f"Bearer {self.sf_client.access_token}",
+            "Content-Type": "application/json",
+        }
+        params = {"q": query}
+
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            logger.info(f"Response: {response}")
+            response.raise_for_status()
+            contacts = response.json()["records"]
+            return contacts
+        except Exception as e:
+            print(f"Failed to retrieve contacts: {e}")
+            return []
+
+        # if response.status_code == 200:
+        #     results = response.json()
+        #     if results["totalSize"] > 0:
+        #         # Contact exists, so update it
+        #         contact_id = results["records"][0]["Id"]
+        #         update_response = await requests.patch(
+        #             f"{customer_base_url}/services/data/v60.0/sobjects/Contact/{contact_id}",
+        #             json=contact_data,
+        #             headers=headers,
+        #         )
+        #         return update_response.json()
+        #     else:
+        #         # Contact does not exist, create a new one
+        #         create_response = await requests.post(
+        #             f"{customer_base_url}/services/data/v60.0/sobjects/Contact/",
+        #             json=contact_data,
+        #             headers=headers,
+        #         )
+        #         return create_response.json()
+        # else:
+        #     # Handle errors
+        #     return response.json()
 
 
 def get_authorization_url(company: str) -> str:
@@ -58,6 +136,7 @@ def get_authorization_url(company: str) -> str:
 
 
 def handle_callback(company: str, response_url: str) -> None:
+    logger.info(f"Started handling callback")
     sf = OAuth2Session(
         client_id=SALESFORCE_CLIENT_ID,
         redirect_uri=SALESFORCE_REDIRECT_URI,
@@ -67,10 +146,23 @@ def handle_callback(company: str, response_url: str) -> None:
         client_secret=SALESFORCE_CLIENT_SECRET,
         authorization_response=response_url,
     )
-
     logger.info(
         f"Salesforce data updated for {company}. Client URL: {token_data['instance_url']}."
     )
+    logger.info(f"About to insert new user to salesforce repository")
+
+    sf_users_repository.insert(
+        uuid=uuid.uuid4(),
+        name="Asaf",
+        company="Definitely not Kubiya.ai",
+        client_url=token_data["instance_url"],
+        refresh_token=token_data["refresh_token"],
+        access_token=token_data["access_token"],
+    )
+
+    logger.info(f"Inserted user to repository")
+
+    return token_data
 
 
 def create_salesforce_client(
