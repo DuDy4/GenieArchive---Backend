6caa87980ddf933a89be0ddcb139b32931890d11
Dan Shevel <dan.shevel@trywonder.ai>
2024-07-15 12:07:43 +0300

    Python file to start the mock api in port 8001

---
+++ start_mock_api.py
@@ -0,0 +1,39 @@
+import os
+
+import uvicorn
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+
+# from starlette_context import middleware, context, plugins
+# from starlette_context.middleware import ContextMiddleware
+
+from data.api.mock_api import v1_router
+
+load_dotenv()
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:1234"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(
+    SessionMiddleware, secret_key=os.environ.get("APP_SECRET_KEY"), max_age=3600
+)
+
+app.include_router(v1_router)
+
+MOCK_PORT = int(os.environ.get("MOCK_PORT", 8001))
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=MOCK_PORT,
+        ssl_keyfile="./key.pem",
+        ssl_certfile="./cert.pem",
+    )
