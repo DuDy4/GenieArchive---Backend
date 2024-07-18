ece9797bd913c9279b113aaca4c77d7dea491ceb
Dan Shevel <dan.shevel@trywonder.ai>
2024-07-18 12:22:21 +0300

    changed the start_api.py to work with http

--- start_api.py
+++ start_api.py
@@ -53,10 +53,4 @@ PORT = int(os.environ.get("PERSON_PORT", 8000))
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=PORT,
-        ssl_keyfile="./key.pem",
-        ssl_certfile="./cert.pem",
-    )
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
