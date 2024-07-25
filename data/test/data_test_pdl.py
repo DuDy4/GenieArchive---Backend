2893b22c9660441bb4328f97a32bd51eddcb9f88
Dan Shevel <dan.shevel@trywonder.ai>
2024-07-25 19:48:15 +0300

    Changed a content in the test - not important

--- data/test/data_test_pdl.py
+++ data/test/data_test_pdl.py
@@ -9,7 +9,7 @@ from data.data_common.events.topics import Topic
 
 def test_new_contacts():
 
-    test_data = """{"uuid": "e58a0044-c276-4c1c-a63e-5a57d79ccb8b", "name": "Asaf Savich", "company": "GenieAI", "email": "asaf@trywonder.ai", "linkedin": "https://www.linkedin.com/in/asaf-savich/", "position": "CTO", "timezone": ""}"""
+    test_data = """{"uuid": "592ff140-91c2-475e-8bc2-1ce23328896a", "name": "Asaf Savich", "company": "GenieAI", "email": "asaf@trywonder.ai", "linkedin": "https://www.linkedin.com/in/asaf-savich/", "position": "CTO", "timezone": ""}"""
 
     event = GenieEvent(
         topic=Topic.NEW_CONTACT_TO_ENRICH, data=test_data, scope="public"
