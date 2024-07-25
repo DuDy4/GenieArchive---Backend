0887d8b9649779cb7f67d001ada2f0fb653218b4
Dan Shevel <dan.shevel@trywonder.ai>
2024-07-25 19:46:40 +0300

    Changed the email creds to use email address as a key instead of the tenant_id

--- data/data_common/repositories/google_creds_repository.py
+++ data/data_common/repositories/google_creds_repository.py
@@ -11,7 +11,6 @@ from loguru import logger
 class GoogleCredsRepository:
     def __init__(self, conn):
         self.conn = conn
-        # self.cursor = conn.cursor()
         self.create_table_if_not_exists()
 
     def __del__(self):
@@ -23,7 +22,7 @@ class GoogleCredsRepository:
         CREATE TABLE IF NOT EXISTS google_creds (
             id SERIAL PRIMARY KEY,
             uuid VARCHAR UNIQUE NOT NULL,
-            tenant_id VARCHAR,
+            email VARCHAR,
             refresh_token TEXT,
             access_token TEXT
         );
@@ -38,13 +37,13 @@ class GoogleCredsRepository:
 
     def insert(self, creds: dict):
         insert_query = """
-        INSERT INTO google_creds (uuid, tenant_id, refresh_token, access_token)
+        INSERT INTO google_creds (uuid, email, refresh_token, access_token)
         VALUES (%s, %s, %s, %s)
         """
-        if self.exists(creds.get("tenantId")):
+        if self.exists(creds.get("email")):
             logger.info(
-                "Google creds already exists in database for tenant_id:",
-                creds.get("tenantId"),
+                "Google creds already exist in database for email:",
+                creds.get("email"),
             )
             self.update_creds(creds)
             return
@@ -57,50 +56,52 @@ class GoogleCredsRepository:
                     insert_query,
                     (
                         uuid,
-                        creds.get("tenant_id"),
+                        creds.get("email"),
                         creds.get("refreshToken"),
                         creds.get("accessToken"),
                     ),
                 )
                 self.conn.commit()
         except Exception as error:
-            logger.error("Error inserting tenant:", error)
+            logger.error("Error inserting credentials:", error)
             self.conn.rollback()
 
-    def exists(self, tenant_id: str) -> bool:
+    def exists(self, email: str) -> bool:
         query = """
-        SELECT * FROM google_creds WHERE tenant_id = %s;
+        SELECT * FROM google_creds WHERE email = %s;
         """
         with self.conn.cursor() as cursor:
-            cursor.execute(query, (tenant_id,))
+            cursor.execute(query, (email,))
             result = cursor.fetchone()
             logger.debug(f"Result: {result}")
             return result is not None
 
-    def get_creds(self, tenant_id: str) -> Union[dict, None]:
+    def get_creds(self, email: str) -> Union[dict, None]:
         query = """
-        SELECT * FROM google_creds WHERE tenant_id = %s;
+        SELECT * FROM google_creds WHERE email = %s;
         """
-        logger.debug(f"About to get creds for tenant: {tenant_id}")
+        logger.debug(f"About to get creds for email: {email}")
         with self.conn.cursor() as cursor:
-            cursor.execute(query, (tenant_id,))
+            cursor.execute(query, (email,))
             creds = cursor.fetchone()
+            if not creds:
+                return None
             creds_dict = {
                 "uuid": creds[1],
-                "tenant_id": creds[2],
+                "email": creds[2],
                 "refresh_token": creds[3],
                 "access_token": creds[4],
             }
             return creds_dict if creds else None
 
-    def has_google_creds(self, tenant_id: str) -> bool:
-        google_creds = self.get_creds(tenant_id)
+    def has_google_creds(self, email: str) -> bool:
+        google_creds = self.get_creds(email)
         return google_creds.get("refresh_token") is not None
 
     def update_creds(self, creds):
         update_query = """
         UPDATE google_creds SET refresh_token = %s, access_token = %s
-        WHERE tenant_id = %s
+        WHERE email = %s
         """
         try:
             with self.conn.cursor() as cursor:
@@ -109,14 +110,15 @@ class GoogleCredsRepository:
                     (
                         creds.get("refreshToken") or creds.get("refresh_token"),
                         creds.get("accessToken") or creds.get("access_token"),
-                        creds.get("tenantId") or creds.get("tenant_id"),
+                        creds.get("email"),
                     ),
                 )
                 self.conn.commit()
+                logger.info("Updated credentials in database")
                 return
         except psycopg2.Error as error:
             self.conn.rollback()
-            logger.error(f"Error updating creds, because: {error.pgerror}")
+            logger.error(f"Error updating credentials, because: {error.pgerror}")
             traceback.print_exc()
         except Exception as e:
             self.conn.rollback()
