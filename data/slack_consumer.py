384c8bafd61fe3cc5896b8543fd30fdcd9bc451b
Dan Shevel <dan.shevel@trywonder.ai>
2024-07-28 21:40:44 +0300

    Fixed names of events-topics, and fixed the empty message bug in slack

--- data/slack_consumer.py
+++ data/slack_consumer.py
@@ -26,7 +26,7 @@ class SlackConsumer(GenieConsumer):
     ):
         super().__init__(
             topics=[
-                Topic.FAILED_TO_GET_DOMAIN_INFO,
+                Topic.FAILED_TO_GET_LINKEDIN_URL,
                 Topic.FAILED_TO_ENRICH_DATA,
             ],
             consumer_group=CONSUMER_GROUP,
@@ -38,29 +38,30 @@ class SlackConsumer(GenieConsumer):
         topic = event.properties.get(b"topic").decode("utf-8")
         logger.info(f"Processing event on topic {topic}")
         match topic:
-            case Topic.FAILED_TO_GET_DOMAIN_INFO:
-                logger.info("Handling failed attempt to get domain info")
-                await self.handle_failed_to_get_domain_info(event)
+            case Topic.FAILED_TO_GET_LINKEDIN_URL:
+                logger.info("Handling failed attempt to get linkedin url")
+                await self.handle_failed_to_get_linkedin_url(event)
             case Topic.FAILED_TO_ENRICH_DATA:
                 logger.info("Handling failed attempt to enrich data")
                 await self.handle_failed_to_enrich_data(event)
             case _:
                 logger.info(f"Unknown topic: {topic}")
 
-    async def handle_failed_to_get_domain_info(self, event):
+    async def handle_failed_to_get_linkedin_url(self, event):
         event_body_str = event.body_as_str()
         event_body = json.loads(event_body_str)
         if isinstance(event_body, str):
             event_body = json.loads(event_body)
         email = event_body.get("email")
         domain = self.company_repository.get_company_from_domain(email.split("@")[1])
-        message = f"""
-        failed to identify info for email: {email}.
-        We know that this domain is associated with a company {domain["name"]}.
-        {'Description: ' + domain["description"] if domain["description"] else ""}
-        {'Technologies: ' + ', '.join(domain["technologies"]) if domain["technologies"] else ""}
-        {'Known employees: ' + ', '.join([f'{employee["name"]} (Position: {employee["position"] if employee["position"] else "Unknown"})' for employee in domain["employees"]]) if domain["employees"] else ""}
-        """
+        message = f"failed to identify info for email: {email}."
+        if domain:
+            message += f"""
+            We know that this domain is associated with a company {domain["name"]}.
+            {'Description: ' + domain["description"] if domain["description"] else ""}
+            {'Technologies: ' + ', '.join(domain["technologies"]) if domain["technologies"] else ""}
+            {'Known employees: ' + ', '.join([f'{employee["name"]} (Position: {employee["position"] if employee["position"] else "Unknown"})' for employee in domain["employees"]]) if domain["employees"] else ""}
+            """
         send_message(message)
 
     async def handle_failed_to_enrich_data(self, event):
