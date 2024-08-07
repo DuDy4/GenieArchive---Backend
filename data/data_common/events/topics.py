37a9a7910d425a7c7a832055ac6051e473caa5d1
Dan Shevel <dan.shevel@trywonder.ai>
2024-08-07 10:06:09 +0300

    Rearranged topic and added explanations

--- data/data_common/events/topics.py
+++ data/data_common/events/topics.py
@@ -1,22 +1,61 @@
 class Topic:
+    # Events that notify about new objects in the system
 
-    ALREADY_FAILED_TO_ENRICH_EMAIL = "already-failed-to-enrich-email"
+    # New objects events
+    NEW_MEETING = "new-meeting"
     NEW_COMPANY_DATA = "new-company-data"
     NEW_PERSON = "new-person"
-    UP_TO_DATE_ENRICHED_DATA = "up-to-date-enriched-data"
-    NEW_EMAIL_ADDRESS_TO_PROCESS = "new-email-address-to-process"
-    NEW_EMAIL_ADDRESS_TO_ENRICH = "new-email-address-to-enrich"
-    NEW_EMAIL_TO_PROCESS_DOMAIN = "new-email-to-process-domain"
-    NEW_LINKEDIN_URL = "new-linkedin-url"
-    FINISHED_NEW_PROFILE = "finished-new-profile"
-    NEW_CONTACT_TO_ENRICH = "new-contact-to-enrich"
-    NEW_CONTACT = "new-contact"
+    NEW_CONTACT = "new-contact"  # Also used for new person
     NEW_INTERACTION = "new-interaction"
-    UPDATED_ENRICHED_DATA = "got-updated-enriched-data"
-    NEW_PERSONAL_DATA = "new-personal-data-to-process"
-    NEW_PROCESSED_PROFILE = "new-processed-profile"
-    NEW_MEETING = "new-meeting"
-    FAILED_TO_ENRICH_DATA = "failed-to-enrich-data"
-    FAILED_TO_ENRICH_EMAIL = "failed-to-enrich-email"
-    FAILED_TO_GET_DOMAIN_INFO = "failed-to-get-domain-info"
-    FAILED_TO_GET_LINKEDIN_URL = "failed-to-get-linkedin-url"
+    NEW_EMAIL_ADDRESS_TO_PROCESS = (
+        "new-email-address-to-process"  # Derived from new meetings
+    )
+    NEW_LINKEDIN_URL = "new-linkedin-url"  # Not implemented yet - should notify about manually added LinkedIn urls
+
+    # Events that require tasks
+
+    # People enrichment
+    NEW_EMAIL_ADDRESS_TO_ENRICH = (
+        "new-email-address-to-enrich"  # Call for people data labs
+    )
+    NEW_CONTACT_TO_ENRICH = "new-contact-to-enrich"  # Call for people data labs
+    UPDATED_ENRICHED_DATA = (
+        "got-updated-enriched-data"  # People data labs succeeded in enriching data
+    )
+    NEW_PERSONAL_DATA = "new-personal-data-to-process"  # PersonManager calls for Langsmith to process enriched data
+    NEW_PROCESSED_PROFILE = (
+        "new-processed-profile"  # Langsmith Succeeded in processing enriched data
+    )
+
+    # Company enrichment
+    NEW_EMAIL_TO_PROCESS_DOMAIN = "new-email-to-process-domain"  # Call for hunter.io
+
+    # Events that stops the process
+
+    # Events that notify that all is well (no action needed or end of successful process)
+    UP_TO_DATE_ENRICHED_DATA = (
+        "up-to-date-enriched-data"  # People data labs already has the latest data
+    )
+    ALREADY_FAILED_TO_ENRICH_EMAIL = (
+        "already-failed-to-enrich-email"  # People data labs already failed to enrich
+    )
+    # email, and not enough time passed to retry
+    COMPANY_NEWS_UPDATED = (
+        "company-news-updated"  # News scrapper succeeded in getting news
+    )
+    FINISHED_NEW_PROFILE = "finished-new-profile"  # PersonManager succeeded working with people data labs + Langsmith
+
+    # Events that notify about failures
+    FAILED_TO_ENRICH_DATA = (
+        "failed-to-enrich-data"  # People data labs failed to enrich data
+    )
+    FAILED_TO_ENRICH_EMAIL = (
+        "failed-to-enrich-email"  # People data labs failed to enrich email
+    )
+    FAILED_TO_GET_DOMAIN_INFO = (
+        "failed-to-get-domain-info"  # Hunter.io failed to get domain info
+    )
+    FAILED_TO_GET_LINKEDIN_URL = "failed-to-get-linkedin-url"  # NOT ACTIVE - Langsmith failed to get LinkedIn url
+    FAILED_TO_GET_COMPANY_NEWS = (
+        "failed-to-get-company-news"  # News scrapper failed to get news
+    )
