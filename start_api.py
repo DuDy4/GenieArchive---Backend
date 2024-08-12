fff9a7684a0dc546eb694e566b2587c19d82df2d
asafwonder <asaf@genieai.ai>
2024-08-13 01:35:05 +0300

    Change cors urls to support wild card 2

--- start_api.py
+++ start_api.py
@@ -40,7 +40,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://*.genieai.ai"],
+    allow_origins=["http://localhost:5173","https://alpha.genieai.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
