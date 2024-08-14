a11d7747382583c26871b76d74dc66a2f6bc2683
Dan Shevel <dan.shevel@trywonder.ai>
2024-08-14 21:23:10 +0300

    Cleanup through new topics

--- app/app_common/events/topics.py
+++
@@ -1,9 +0,0 @@
-class Topic:
-    FINISHED_NEW_PROFILE = "finished-new-profile"
-    NEW_CONTACT_TO_ENRICH = "new-contact-to-enrich"
-    NEW_CONTACT = "new-contact"
-    NEW_INTERACTION = "new-interaction"
-    UPDATED_ENRICHED_DATA = "got-updated-enriched-data"
-    NEW_PERSONAL_DATA = "new-personal-data-to-process"
-    NEW_PROCESSED_DATA = "new-processed-data"
-    NEW_PROCESSED_PROFILE = "new-processed-profile"
