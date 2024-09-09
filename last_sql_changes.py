2c53845704a5d38a7e2863928a9bed4e4145e296
Dan Shevel <dan.shevel@trywonder.ai>
2024-09-09 13:02:13 +0300

    added timeout to last_sql_changes

--- last_sql_changes.py
+++ last_sql_changes.py
@@ -2,6 +2,7 @@ import traceback
 from data.data_common.utils.postgres_connector import get_db_connection
 from common.genie_logger import GenieLogger
 import psycopg2
+from psycopg2 import sql
 
 logger = GenieLogger()
 
@@ -14,11 +15,18 @@ ADD COLUMN classification VARCHAR;
 
 try:
     logger.debug(f"About to execute command: {alter_command}")
+    conn.autocommit = False  # Disable autocommit mode for better control
     with conn.cursor() as cursor:
+        # Increase the statement timeout to 30 seconds (30000 milliseconds)
+        cursor.execute(sql.SQL("SET statement_timeout TO 30000;"))
         cursor.execute(alter_command)
     logger.debug("Command executed successfully. About to commit command.")
     conn.commit()
     logger.debug("Command committed successfully.")
+
+except psycopg2.OperationalError as e:
+    logger.error(f"Operational error with the database: {e}")
+    conn.rollback()
 
 except psycopg2.Error as e:
     logger.error(f"Error executing SQL command: {e}")
@@ -31,4 +39,5 @@ except Exception as e:
 
 finally:
     logger.debug("Closing the database connection.")
-    conn.close()
+    if conn:
+        conn.close()
