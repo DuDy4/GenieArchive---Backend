fd653ef5035f4fbc134873439a5d4c34aa7597a1
Dan Shevel <dan.shevel@trywonder.ai>
2024-10-07 15:02:26 +0300

    Made sales_material_consumer save file details to db, and start embed document only if file is new

--- data/api/api_services_classes/user_materials_services.py
+++ data/api/api_services_classes/user_materials_services.py
@@ -10,7 +10,11 @@ from data.data_common.dependencies.dependencies import (
 from common.genie_logger import GenieLogger
 from data.internal_services.files_upload_service import FileUploadService
 from fastapi import HTTPException
-from data.data_common.utils.str_utils import upload_file_name_validation, ALLOWED_EXTENSIONS, MAX_FILE_NAME_LENGTH
+from data.data_common.utils.str_utils import (
+    upload_file_name_validation,
+    ALLOWED_EXTENSIONS,
+    MAX_FILE_NAME_LENGTH,
+)
 from data.data_common.events.genie_event import GenieEvent
 from data.data_common.events.topics import Topic
 
@@ -21,31 +25,43 @@ class UserMaterialServices:
     def __init__(self):
         self.tenants_repository = tenants_repository()
 
-
     def file_uploaded(self, uploaded_files):
         logger.info(f"Event details: {uploaded_files}")
         for file in uploaded_files:
-            file_data = file['data']
+            file_data = file["data"]
             if not file_data:
                 logger.error(f"Data not found in azure event {uploaded_files}")
                 continue
             logger.info(f"Event data: {file_data}")
-            user_email =  email_utils.extract_email_from_url(file_data['blobUrl'])
+            user_email = email_utils.extract_email_from_url(file_data["blobUrl"])
             if not user_email:
                 logger.error(f"User email is not part of the blob")
                 continue
             tenant_id = self.tenants_repository.get_tenant_id_by_email(user_email)
             if not tenant_id:
-                logger.error(f"Teanant ID not found the email: {user_email}")
+                logger.error(f"Tenant ID not found the email: {user_email}")
                 continue
+            logger.info(f"File: {file}")
+            upload_time = file.get("eventTime")
+            logger.info(f"Upload time: {upload_time}")
+            if not upload_time:
+                logger.error(f"Upload time not found in the event data")
             logger.set_tenant_id(tenant_id)
-            metadata = {"id": file['id'], "user": user_email, "tenant_id" : tenant_id, "type" : "uploaded_file"} 
-            GenieEvent(Topic.FILE_UPLOADED, {'event_data': file_data, 'metadata' : metadata}, "public").send()
-
+            metadata = {
+                "id": file["id"],
+                "user": user_email,
+                "tenant_id": tenant_id,
+                "type": "uploaded_file",
+                "upload_time": upload_time,
+            }
+            GenieEvent(Topic.FILE_UPLOADED, {"event_data": file_data, "metadata": metadata}, "public").send()
 
     def generate_upload_url(self, tenant_id, file_name):
         if not upload_file_name_validation(file_name):
-            raise HTTPException(status_code=400, detail=f"File name not supported. Must be: 1. Less than {MAX_FILE_NAME_LENGTH} characters 2. No special characters. 3. Only extensions supported: {ALLOWED_EXTENSIONS}")
+            raise HTTPException(
+                status_code=400,
+                detail=f"File name not supported. Must be: 1. Less than {MAX_FILE_NAME_LENGTH} characters 2. No special characters. 3. Only extensions supported: {ALLOWED_EXTENSIONS}",
+            )
 
         upload_url = FileUploadService.generate_upload_url(tenant_id, file_name)
         logger.info(f"Succesfullly create update url: {upload_url}")
