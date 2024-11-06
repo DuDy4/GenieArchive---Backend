5776949e90c0e6b8651890952cf4cf14d9585efb
Dan Shevel <dan.shevel@trywonder.ai>
2024-11-06 11:47:39 +0200

    Fixed bug in string var

--- data/tasks/send_reminder_email.py
+++ data/tasks/send_reminder_email.py
@@ -36,24 +36,19 @@ def run():
     for meeting in meetings_to_send_reminders:
         # Convert and log the reminder schedule to check if it matches the current time
         reminder_schedule = next_meeting_reminder_time_utc
-        reminder_schedule_utc = datetime.fromisoformat(reminder_schedule).replace(tzinfo=timezone.utc)
 
-        logger.info(f"Evaluating meeting for reminder: {meeting.subject}, Start Time (UTC): {meeting.start_time}, "
-                    f"Reminder Schedule (UTC): {reminder_schedule_utc}, Classification: {meeting.classification.value}")
+        logger.info(f"Evaluating meeting for reminder: {meeting.subject}, Start Time (UTC): {meeting.start_time}, Classification: {meeting.classification.value}")
 
-        if reminder_schedule_utc <= current_utc_time:
-            logger.info(f"Sending reminder for meeting: {meeting.subject}")
-            event = GenieEvent(
-                topic=Topic.NEW_UPCOMING_MEETING,
-                data={"meeting_uuid": meeting.uuid},
-            )
-            try:
-                event.send()
-                logger.info(f"Reminder sent successfully for meeting: {meeting.subject}, meeting UUID: {meeting.uuid}")
-            except Exception as e:
-                logger.error(f"Failed to send reminder for meeting: {meeting.subject}, meeting UUID: {meeting.uuid}. Error: {str(e)}")
-        else:
-            logger.info(f"Reminder not yet due for meeting: {meeting.subject}. Scheduled reminder time: {reminder_schedule_utc}, Current UTC time: {current_utc_time}")
+        logger.info(f"Sending reminder for meeting: {meeting.subject}")
+        event = GenieEvent(
+            topic=Topic.NEW_UPCOMING_MEETING,
+            data={"meeting_uuid": meeting.uuid},
+        )
+        try:
+            event.send()
+            logger.info(f"Reminder sent successfully for meeting: {meeting.subject}, meeting UUID: {meeting.uuid}")
+        except Exception as e:
+            logger.error(f"Failed to send reminder for meeting: {meeting.subject}, meeting UUID: {meeting.uuid}. Error: {str(e)}")
 
     logger.info("Completed send_reminder_email task")
 
