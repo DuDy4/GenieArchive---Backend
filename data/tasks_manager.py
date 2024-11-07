4764944e14aaa11a85121862969f3f023eaaf884
Dan Shevel <dan.shevel@trywonder.ai>
2024-11-07 16:46:25 +0200

    Every hour should save all relevant profile pictures to azure storage

--- data/tasks_manager.py
+++ data/tasks_manager.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.date import DateTrigger
 import logging
 import time
-from data.tasks import send_reminder_email  # Import the task module
+from data.tasks import send_reminder_email, upload_profile_picture_to_blob
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -16,30 +15,32 @@ class SchedulerService:
         self.scheduler = BackgroundScheduler()
         self.scheduler.start()
 
-    def add_task(self, task_module, immediate=False):
+    def add_task(self, task_module, interval_minutes=15, immediate=False):
         """
         Schedule the task with fixed intervals and optionally run immediately.
 
         :param task_module: The task module to run
+        :param interval_minutes: Interval in minutes for running the task
         :param immediate: Whether to run the task immediately on startup
         """
-        # Run the task immediately on startup if `immediate` is True
         if immediate:
             self.scheduler.add_job(task_module.run, DateTrigger(run_date=datetime.now()))
 
-        # Schedule the task for every 15 minutes at fixed intervals
-        cron_trigger = CronTrigger(minute="*/5")
+        cron_trigger = CronTrigger(minute=f"*/{interval_minutes}")
         self.scheduler.add_job(task_module.run, cron_trigger, id=task_module.__name__, replace_existing=True)
 
-        logger.info(f"Scheduled task '{task_module.__name__}' for every 15 minutes at fixed intervals.")
+        logger.info(f"Scheduled task '{task_module.__name__}' for every {interval_minutes} minutes.")
 
     def stop(self):
         self.scheduler.shutdown()
 
 if __name__ == "__main__":
     scheduler_service = SchedulerService()
-    # Add the send_reminder_email task with immediate start and fixed interval
-    scheduler_service.add_task(send_reminder_email, immediate=True)
+    # Add the send_reminder_email task with immediate start and a 15-minute interval
+    scheduler_service.add_task(send_reminder_email, interval_minutes=15, immediate=True)
+
+    # Add the other_task to run every hour
+    scheduler_service.add_task(upload_profile_picture_to_blob, interval_minutes=60, immediate=True)
 
     try:
         while True:
