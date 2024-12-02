c6bcc915d7a830549ce865e2085b43c60647f3a0
Dan Shevel <dan.shevel@trywonder.ai>
2024-12-02 16:45:06 +0200

    Fixed script to scrap linkedin posts for future profile

--- data/internal_scripts/fetch_social_media_news.py
+++ data/internal_scripts/fetch_social_media_news.py
@@ -23,6 +23,10 @@ def get_all_uuids_that_should_try_posts():
     all_personal_data_uuid = personal_data_repository.get_all_uuids_that_should_try_fetch_posts()
     return all_personal_data_uuid
 
+def get_all_future_uuids_that_should_try_posts():
+    future_emails = personal_data_repository.get_future_profiles_without_news()
+    return future_emails
+
 
 def fetch_linkedin_posts(uuids: list, scrap_num=5):
     for uuid in uuids[:scrap_num]:
@@ -33,9 +37,10 @@ def fetch_linkedin_posts(uuids: list, scrap_num=5):
                 continue
             logger.info(f"Fetching posts for {linkedin_url}")
 
-            should_fetch = personal_data_repository.should_do_linkedin_posts_lookup(uuid)
-            logger.info(f"Should fetch posts: {should_fetch}")
-            if not should_fetch:
+            # should_fetch = personal_data_repository.should_do_linkedin_posts_lookup(uuid)
+            should_not_fetch = personal_data_repository.get_news_data_by_uuid(uuid)
+            logger.info(f"Should fetch posts: {should_not_fetch}")
+            if should_not_fetch:
                 logger.info(f"Skipping fetching posts for {linkedin_url}")
                 continue
 
@@ -48,7 +53,7 @@ def fetch_linkedin_posts(uuids: list, scrap_num=5):
                 personal_data_repository.update_news_to_db(
                     uuid, None, PersonalDataRepository.TRIED_BUT_FAILED
                 )
-                return {"error": "No posts found or an error occurred"}
+                logger.error(f"Updated status to TRIED_BUT_FAILED for {linkedin_url}")
 
             logger.info(f"Successfully scraped {len(scraped_posts)} posts from LinkedIn URL: {linkedin_url}")
             news_data_objects = []
@@ -65,10 +70,9 @@ def fetch_linkedin_posts(uuids: list, scrap_num=5):
             final_news_data_list = list(set(news_data_objects + news_in_database))
             if final_news_data_list:
                 personal_data_repository.update_news_list_to_db(uuid, final_news_data_list, PersonalDataRepository.FETCHED)
-            if news_data_objects:
-                event = GenieEvent(Topic.NEW_PERSONAL_DATA, {"person_uuid": uuid, "force": True}, "public")
-                event.send()
-            return {"posts": news_data_objects}
+            # if news_data_objects:
+            #     event = GenieEvent(Topic.NEW_PERSONAL_DATA, {"person_uuid": uuid, "force": True}, "public")
+            #     event.send()
         except requests.exceptions.HTTPError as http_err:
             logger.error(f"HTTP error occurred: {http_err}")
             news_in_db = personal_data_repository.get_news_data_by_uuid(uuid)
@@ -89,8 +93,16 @@ def create_summary_to_existing_posts(uuids: list):
         news_data_objects = [news.to_dict() if not news.summary else None for news in news_data_objects]
         news_data_objects = [news for news in news_data_objects if news]
         event = GenieEvent(
-            Topic.NEW_NEWS_DATA,
+            Topic.NEW_PERSONAL_DATA,
             data={"uuid": uuid, "news_data": news_data_objects},
         )
         event.send()
         logger.info(f"Successfully sent event for {uuid}")
+
+
+if __name__ == "__main__":
+    # all_uuids = get_all_uuids_that_should_try_posts()
+    uuids = get_all_future_uuids_that_should_try_posts()
+    logger.info(f"Fetching posts for {len(uuids)} profiles")
+    posts = fetch_linkedin_posts(uuids, 13)
+    # create_summary_to_existing_posts(uuids)
