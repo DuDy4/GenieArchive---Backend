38b878da1d3081a9cf9d6320eb8c289c1a283abe
Dan Shevel <dan.shevel@trywonder.ai>
2024-12-12 18:24:44 +0200

    Better news_utils filtering

--- common/utils/news_utils.py
+++ common/utils/news_utils.py
@@ -4,14 +4,5 @@ logger = GenieLogger()
 
 
 def filter_not_reshared_social_media_news(news: list, linkedin_url):
-    if "https://www." not in linkedin_url:
-        linkedin_url = f"https://www.{linkedin_url}"
-    not_reshared_news = [new for new in news if new.reshared == linkedin_url]
+    not_reshared_news = [new for new in news if linkedin_url in new.reshared]
     return not_reshared_news
-    # reshared = [news.reshared for news in news if news.reshared != linkedin_url]
-    # reshared = []
-    # for news in news:
-    #     logger.info(f"News reshared: {news.reshared} and linkedin_url: {linkedin_url}")
-    #     if news.reshared != linkedin_url:
-    #         reshared.append(news.reshared)
-    # return reshared
