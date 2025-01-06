8f87b8e66050c8f27a05c3d819fe552b1614c1a3
Dan Shevel <dan.shevel@trywonder.ai>
2025-01-06 13:46:04 +0200

    Working statuses with user_id

--- data/data_common/data_transfer_objects/status_dto.py
+++ data/data_common/data_transfer_objects/status_dto.py
@@ -22,23 +22,25 @@ class StatusDTO(BaseModel):
     ctx_id: str
     object_id: str
     object_type: Optional[str | StatusTypeEnum] = None
+    user_id: str
     tenant_id: str
     event_topic: str
     previous_event_topic: Optional[str | None] = None
     current_event_start_time: datetime
     status: StatusEnum
 
-    @field_validator("ctx_id", "object_id", "tenant_id")
+    @field_validator("ctx_id", "object_id", "user_id")
     def not_empty(cls, value):
         if not str(value).strip():
             raise ValueError("Field cannot be empty or whitespace")
         return value
 
-    def to_tuple(self) -> Tuple[str, str, str, str, str, str | None, str, str]:
+    def to_tuple(self) -> Tuple[str, str, str, str, str, str, str | None, str, str]:
         return (
             self.ctx_id,
             self.object_id,
             self.object_type,
+            self.user_id,
             self.tenant_id,
             self.event_topic,
             self.previous_event_topic,
@@ -47,16 +49,17 @@ class StatusDTO(BaseModel):
         )
 
     @classmethod
-    def from_tuple(cls, data: Tuple[str, str, str,str, str, str | None, str, str]) -> "StatusDTO":
+    def from_tuple(cls, data: Tuple[str, str, str, str,str, str, str | None, str, str]) -> "StatusDTO":
         return cls(
             ctx_id=data[0],
             object_id=data[1],
             object_type=StatusTypeEnum(data[2]) or data[2],
-            tenant_id=data[3],
-            event_topic=data[4],
-            previous_event_topic=data[5],
-            current_event_start_time=datetime.fromisoformat(data[6]) if isinstance(data[6], str) else data[6],
-            status=StatusEnum(data[7])
+            user_id=data[3],
+            tenant_id=data[4],
+            event_topic=data[5],
+            previous_event_topic=data[6],
+            current_event_start_time=datetime.fromisoformat(data[7]) if isinstance(data[7], str) else data[7],
+            status=StatusEnum(data[8])
         )
 
     def to_dict(self) -> Dict[str, Any]:
@@ -64,6 +67,7 @@ class StatusDTO(BaseModel):
             "ctx_id": self.ctx_id,
             "object_id": str(self.object_id),
             "object_type": self.object_type,
+            "user_id": self.user_id,
             "tenant_id": self.tenant_id,
             "event_topic": self.event_topic,
             "previous_event_topic": self.previous_event_topic,
@@ -77,6 +81,7 @@ class StatusDTO(BaseModel):
             ctx_id=data["ctx_id"],
             object_id=data["object_id"],
             object_type=StatusTypeEnum(data.get("object_type")) or data.get("object_type"),
+            user_id=data["user_id"],
             tenant_id=data["tenant_id"],
             event_topic=data["event_topic"],
             previous_event_topic=data["previous_event_topic"],
--- data/data_common/repositories/statuses_repository.py
+++ data/data_common/repositories/statuses_repository.py
@@ -3,7 +3,7 @@ from dbm import error
 from uuid import UUID
 import psycopg2
 import traceback
-from common.genie_logger import GenieLogger
+from common.genie_logger import GenieLogger, user_id
 from data.data_common.utils.postgres_connector import db_connection
 from data.data_common.data_transfer_objects.status_dto import StatusDTO, StatusEnum
 
@@ -20,6 +20,7 @@ class StatusesRepository:
                 ctx_id VARCHAR,
                 object_id VARCHAR NOT NULL,
                 object_type VARCHAR,
+                user_id VARCHAR NOT NULL,
                 tenant_id VARCHAR NOT NULL,
                 event_topic VARCHAR,
                 previous_event_topic VARCHAR,
@@ -38,15 +39,15 @@ class StatusesRepository:
                 traceback.format_exc()
 
 
-    def start_status(self, ctx_id: str, object_id: str, object_type: str, tenant_id: str, previous_event_topic: str, next_event_topic: str):
+    def start_status(self, ctx_id: str, object_id: str, object_type: str, user_id: str, tenant_id: str, previous_event_topic: str, next_event_topic: str):
         """
         This function should happen on the creation of event, when it is sent to eventhub (before the consumer processes it)
         """
         insert_query = """
-            INSERT INTO statuses (ctx_id, object_id, object_type, tenant_id, event_topic, previous_event_topic, current_event_start_time, status)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+            INSERT INTO statuses (ctx_id, object_id, object_type, user_id, tenant_id, event_topic, previous_event_topic, current_event_start_time, status)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
         """
-        args = (str(ctx_id), object_id, object_type, tenant_id, next_event_topic, previous_event_topic, datetime.now(timezone.utc), StatusEnum.STARTED.value)
+        args = (str(ctx_id), object_id, object_type, user_id, tenant_id, next_event_topic, previous_event_topic, datetime.now(timezone.utc), StatusEnum.STARTED.value)
         logger.info(f"Starting status: {args}")
         with db_connection() as conn:
             try:
@@ -58,13 +59,13 @@ class StatusesRepository:
                 traceback.format_exc()
 
 
-    def update_status(self, ctx_id:str, object_id: str, tenant_id: str, event_topic: str, status: StatusEnum, error_message: str = None):
+    def update_status(self, ctx_id:str, object_id: str, user_id: str, event_topic: str, status: StatusEnum, error_message: str = None):
         query = """
             UPDATE statuses
             SET current_event_start_time = %s, status = %s, error_message = %s
-            WHERE ctx_id = %s AND object_id = %s AND tenant_id = %s AND event_topic = %s;
+            WHERE ctx_id = %s AND object_id = %s AND user_id = %s AND event_topic = %s;
         """
-        args = (datetime.now(timezone.utc), status.value, error_message, str(ctx_id), object_id, tenant_id, event_topic)
+        args = (datetime.now(timezone.utc), status.value, error_message, str(ctx_id), object_id, user_id, event_topic)
         with db_connection() as conn:
             try:
                 with conn.cursor() as cursor:
@@ -75,15 +76,15 @@ class StatusesRepository:
                 traceback.format_exc()
 
 
-    def _exists(self, ctx_id: str, object_id: str, tenant_id: str, topic: str) -> bool:
+    def _exists(self, ctx_id: str, object_id: str, user_id: str, topic: str) -> bool:
         query = """
             SELECT EXISTS(SELECT 1 FROM statuses 
-                          WHERE ctx_id = %s AND object_id = %s AND tenant_id = %s and event_topic = %s);
+                          WHERE ctx_id = %s AND object_id = %s AND user_id = %s and event_topic = %s);
         """
         with db_connection() as conn:
             try:
                 with conn.cursor() as cursor:
-                    cursor.execute(query, (ctx_id, object_id, tenant_id, topic))
+                    cursor.execute(query, (ctx_id, object_id, user_id, topic))
                     result = cursor.fetchone()
                     return result[0]
             except psycopg2.Error as error:
@@ -91,58 +92,58 @@ class StatusesRepository:
                 traceback.format_exc()
 
 
-    def get_status(self, ctx_id: str, object_id: str, tenant_id: str, event_topic: str) -> StatusDTO:
+    def get_status(self, ctx_id: str, object_id: str, user_id: str, event_topic: str) -> StatusDTO:
         query = """
-            SELECT ctx_id, object_id, object_type, tenant_id, event_topic, previous_event_topic, current_event_start_time, status
-            FROM statuses WHERE ctx_id = %s AND object_id = %s AND tenant_id = %s AND event_topic = %s;
+            SELECT ctx_id, object_id, object_type, user_id, tenant_id, event_topic, previous_event_topic, current_event_start_time, status
+            FROM statuses WHERE ctx_id = %s AND object_id = %s AND user_id = %s AND event_topic = %s;
         """
-        logger.info(f"Fetching status for object_id={object_id} and tenant_id={tenant_id}")
+        logger.info(f"Fetching status for object_id={object_id} and user_id={user_id}")
         with db_connection() as conn:
             try:
                 with conn.cursor() as cursor:
-                    cursor.execute(query, (str(ctx_id), object_id, tenant_id, event_topic))
+                    cursor.execute(query, (str(ctx_id), object_id, user_id, event_topic))
                     result = cursor.fetchone()
                     if result:
                         return StatusDTO.from_tuple(result)
                     else:
-                        logger.info(f"No status found for object_id={object_id} and tenant_id={tenant_id}")
+                        logger.info(f"No status found for object_id={object_id} and user_id={user_id}")
                         return None
             except psycopg2.Error as error:
                 logger.error(f"Error fetching status: {error.pgerror}")
                 traceback.format_exc()
 
-    def get_error_message(self, ctx_id: str, object_id: str, tenant_id: str, event_topic: str) -> str:
+    def get_error_message(self, ctx_id: str, object_id: str, user_id: str, event_topic: str) -> str:
         query = """
-            SELECT error_message FROM statuses WHERE ctx_id = %s AND object_id = %s AND tenant_id = %s AND event_topic = %s;
+            SELECT error_message FROM statuses WHERE ctx_id = %s AND object_id = %s AND user_id = %s AND event_topic = %s;
         """
         with db_connection() as conn:
             try:
                 with conn.cursor() as cursor:
-                    cursor.execute(query, (ctx_id, object_id, tenant_id, event_topic))
+                    cursor.execute(query, (ctx_id, object_id, user_id, event_topic))
                     result = cursor.fetchone()
                     if result:
                         logger.info(f"Error message fetched: {result}")
                         return result[0]
                     else:
-                        logger.info(f"No error message found for object_id={object_id} and tenant_id={tenant_id}")
+                        logger.info(f"No error message found for object_id={object_id} and user_id={user_id}")
                         return None
             except psycopg2.Error as error:
                 logger.error(f"Error fetching error message: {error.pgerror}")
                 traceback.format_exc()
 
-    def delete_status(self, ctx_id: str, object_id: str, tenant_id: str, event_topic: str):
+    def delete_status(self, ctx_id: str, object_id: str, user_id: str, event_topic: str):
         query = """
-            DELETE FROM statuses WHERE ctx_id = %s AND object_id = %s AND tenant_id = %s AND event_topic = %s;
+            DELETE FROM statuses WHERE ctx_id = %s AND object_id = %s AND user_id = %s AND event_topic = %s;
         """
-        logger.info(f"Deleting status for object_id={object_id} and tenant_id={tenant_id} and event_topic={event_topic}")
+        logger.info(f"Deleting status for object_id={object_id} and user_id={user_id} and event_topic={event_topic}")
         with db_connection() as conn:
             try:
                 with conn.cursor() as cursor:
-                    cursor.execute(query, (ctx_id, object_id, tenant_id, event_topic))
+                    cursor.execute(query, (ctx_id, object_id, user_id, event_topic))
                     conn.commit()
                     logger.info(f"Rows affected: {cursor.rowcount}")
                     if cursor.rowcount == 0:
-                        logger.warning(f"No status found to delete for object_id={object_id}, tenant_id={tenant_id}, event_topic={event_topic}")
+                        logger.warning(f"No status found to delete for object_id={object_id}, user_id={user_id}, event_topic={event_topic}")
             except psycopg2.Error as error:
                 logger.error(f"Error deleting status: {error}")
                 logger.error(traceback.format_exc())
